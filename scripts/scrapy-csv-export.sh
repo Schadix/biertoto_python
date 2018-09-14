a0563c8f8fb9c1ee6fd208ad1d9ebfbd6b5da5c2
schadix <schadix@gmail.com>
2018-09-14 12:05:15 -0700

    output folder

--- scripts/scrapy-csv-export.sh
+++ scripts/scrapy-csv-export.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
 . ../env/credentials
 
+if [ "$#" -ne 1 ]; then
+    echo "usage: $0 <spieltag>"
+    exit 1
+fi
+
 cd /Users/schadem/code/github/schadix/biertoto_python/biertoto/biertoto
 export PYTHONPATH=`pwd`
 
-
-scrapy crawl biertoto -a spieltag=1 -a username=$USERNAME -a password=$PASSWORD -o spieltag-export-2.csv -t csv
+scrapy crawl biertoto -a spieltag=$1 -a username=$USERNAME -a password=$PASSWORD -o ../../../output/spieltag-export-2.csv -t csv
 
